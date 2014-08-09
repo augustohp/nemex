45dd8a7ea187749097f13f933cf146d946e65a70
Augusto Pascutti <augusto@phpsp.org.br>
2014-08-09 20:52:26 -0300

    Delete node logs errors.

--- php/deleteNode.php
+++ php/deleteNode.php
@@ -1,33 +1,46 @@
 <?php
 	require __DIR__.'/../bootstrap.php';
 
+$errorResponse = [];
 
-	if(!empty($_POST['itemId'])){
+if (empty($_POST['itemId'])) {
+    $errorResponse = [
+        'error' => true,
+        'message' => '"itemId" is required for node removal.'
+    ];
+}
 
-		$Project = new Nemex\Legacy\Project($_POST['project'], '1');
+if (empty($_POST['project'])) {
+    $errorResponse = [
+        'error' => true,
+        'message' => '"project" is required for node removal.'
+    ];
+}
 
-		if (strlen($Project->getName())) {
+if (count($errorResponse)) {
+    echo json_encode($errorResponse);
+    error_log($errorResponse['message']);
+    return;
+}
 
-			$expected_base_dir = realpath(NEMEX_PATH.'projects/' . $Project->getName());
+$Project = new Nemex\Legacy\Project($_POST['project'], '1');
 
-			$possible_item_paths = [];
-			$possible_item_paths[] = realpath($expected_base_dir . '/' . $_POST['itemId']);
-			$possible_item_paths[] = realpath($expected_base_dir . '/big/' . $_POST['itemId']);
+if (strlen($Project->getName())) {
 
-			foreach ($possible_item_paths AS $possible_item_path) {
-				if ($possible_item_path) {
-					$is_in_target_folder = substr($possible_item_path, 0, strlen($expected_base_dir)) === $expected_base_dir;
+    $expected_base_dir = realpath(NEMEX_PATH.'projects/' . $Project->getName());
+    $possible_item_paths = [];
+    $possible_item_paths[] = realpath($expected_base_dir . '/' . $_POST['itemId']);
+    $possible_item_paths[] = realpath($expected_base_dir . '/big/' . $_POST['itemId']);
 
-					if ($is_in_target_folder) {
-						unlink($possible_item_path);
-					}
-				}
-			}
+    foreach ($possible_item_paths AS $possible_item_path) {
+        if (empty($possible_item_path)) {
+            continue;
+        }
 
-			// debug($possible_item_paths);
-		}
+        if (false === unlink($possible_item_path)) {
+            error_log('Could not remove file: '.$possible_item_path);
+        }
 
-	} else {
-		echo "error";
-	}
-
+        error_log('Removed file: '.$possible_item_path);
+    }
+}
