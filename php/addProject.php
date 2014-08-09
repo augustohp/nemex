b847fca287051b254c1773d7e09dccd7beeded9c
Augusto Pascutti <augusto@phpsp.org.br>
2014-08-09 20:52:55 -0300

    New project logs errors.

--- php/addProject.php
+++ php/addProject.php
@@ -1,36 +1,18 @@
 <?php
-	require __DIR__.'/../bootstrap.php';
+require __DIR__.'/../bootstrap.php';
 
-	if(!empty($_POST['newProject'])){
+if (empty($_POST['newProject'])) {
+    throw new Exception('Project name not sent.');
+}
 
-		$newp = clearUTF(preg_replace('/\s+/', '', $_POST['newProject']));
-		$newp = substr(preg_replace('/[^\p{L}\p{N}\s]/u', '', $newp), 0, 18);
+$newp = clearUTF(preg_replace('/\s+/', '', $_POST['newProject']));
+$newp = substr(preg_replace('/[^\p{L}\p{N}\s]/u', '', $newp), 0, 18);
 
-		if (strlen($newp) AND ! file_exists(NEMEX_PATH.'projects/'.$newp)) {
-			$redirect = '<?php
-				header("HTTP/1.0 404 Not Found");
-				?>';
-
-		    mkdir(NEMEX_PATH.'projects/'.$newp, 0777, true);
-		 	$old = umask(0);
-			chmod(NEMEX_PATH.'projects/'.$newp, 0777);
-			file_put_contents(NEMEX_PATH.'projects/'.$newp.'/index.php', $redirect);
-			umask($old);
-
-
-		 	mkdir(NEMEX_PATH.'projects/'.$newp.'/big', 0777, true);
-
-
-		 	$old = umask(0);
-			chmod(NEMEX_PATH.'projects/'.$newp.'/big', 0777);
-			file_put_contents(NEMEX_PATH.'projects/'.$newp.'/big/index.php', $redirect);
-			umask($old);
-
-
-		} else {
-			echo "no";
-		}
-
-	} else {
-		echo "error";
-	}
+mkdir(NEMEX_PATH.'projects/'.$newp, 0777, true);
+$old = umask(0);
+chmod(NEMEX_PATH.'projects/'.$newp, 0777);
+umask($old);
+mkdir(NEMEX_PATH.'projects/'.$newp.'/big', 0777, true);
+$old = umask(0);
+chmod(NEMEX_PATH.'projects/'.$newp.'/big', 0777);
+umask($old);
