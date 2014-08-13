afca9d571783b5aca8ba6dbd8a970929f916c7e7
Augusto Pascutti <augusto@phpsp.org.br>
2014-08-13 01:24:33 -0300

    Removes unecessary files for download of asserts.

--- php/downloadProject.php
+++
@@ -1,6 +0,0 @@
-		header("Content-type: application/zip"); 
-		header("Content-Disposition: attachment; filename=$archive_file_name");
-		header("Content-length: " . filesize($archive_file_name));
-		header("Pragma: no-cache"); 
-		header("Expires: 0"); 
-		readfile("$archive_file_name");
